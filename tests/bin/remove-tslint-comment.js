e4b7da02ab2bef68e6c9503f1f4c64f013be6df1
weiran.zsd <weiran.zsd@alibaba-inc.com>
2019-02-27 22:18:22 +0800

    chore: ignore tmp file & rename files

--- tests/bin/remove-tslint-comment.js
+++ tests/bin/remove-tslint-comment.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 describe('cli', () => {
   it('cli should work!', () => {
     const out = fs.readFileSync(path.join(__dirname, '../fixtures/input-out.js'), 'utf-8');
-    const expected = fs.readFileSync(path.join(__dirname, '../fixtures/output.js'), 'utf-8');
+    const expected = fs.readFileSync(path.join(__dirname, '../fixtures/expected.js'), 'utf-8');
 
     assert.strictEqual(out, expected);
   });
